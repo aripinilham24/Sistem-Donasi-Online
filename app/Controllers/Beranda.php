0319b3cb2bbfc29a25d2b1808b34a7761af879ce
Aripin Ilham <muhamadarifinilham24@gmail.com>
2025-05-17 23:56:47 +0700

    fix the bug in dana card kampanye

--- app/Controllers/Beranda.php
+++ app/Controllers/Beranda.php
@@ -15,25 +15,17 @@ class Beranda extends BaseController
     }
     public function index()
     {
-        // $model = new Mdonasi();
-        // $persentase = $model->getPercentage();
-        // $data['donasi'] = [
-        //     'donasi' => $model->findAll(),
-        //     'persentase' => $persentase,
-        //     'width' => round($persentase)
-        // ];
         $data['donasi'] = $this->model->getAllWithPercentage();
         echo view('user/template/head.php', $data);
         echo view('user/template/header.php', $data);
         echo view('user/template/content.php', $data);
         echo view('user/template/footer.php', $data);
-        // echo dd($data);
     }
 
     public function detail_kampanye($id)
     {
         $data['data'] = $this->model->find($id);
-        // echo dd($data);
+
         echo view('user/template/head.php', $data);
         echo view('user/template/header.php', $data);
         echo view('user/detail_kampanye.php', $data);
