3b0f0979ef76fec582987eff4fe3c36bd148cb8d
Aripin Ilham <muhamadarifinilham24@gmail.com>
2025-05-29 03:44:34 +0700

    update layout

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -12,24 +12,24 @@
     <title>HopeFund - Login</title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css')?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="<?= base_url('assets/css/sb-admin-2.min.css')?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/sb-admin-2.min.css') ?>" rel="stylesheet">
 
 </head>
 
 <body class="bg-gradient-primary">
 
     <div class="container">
-    <?php if (session()->getFlashdata('error')): ?>
-    <div class="alert alert-danger">
-        <?= session()->getFlashdata('error') ?>
-    </div>
-<?php endif; ?>
+        <?php if (session()->getFlashdata('error')): ?>
+            <div class="alert alert-danger">
+                <?= session()->getFlashdata('error') ?>
+            </div>
+        <?php endif; ?>
 
         <!-- Outer Row -->
         <div class="row justify-content-center">
@@ -39,10 +39,7 @@
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6 bg-login-image d-flex align-items-center justify-content-center border">
-                               <h1 class="fw-bold" style="font-size: 5rem;">HopeFund</h1>
-                            </div>
+                        <div class="row justify-content-center">
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
@@ -50,14 +47,13 @@
                                     </div>
                                     <form class="user" action="<?= site_url('Auth/proses_login') ?>" method="post">
                                         <div class="form-group">
-                                            <input type="text" class="form-control form-control-user"
-                                                id="username" name="username" aria-describedby="emailHelp"
+                                            <input type="text" class="form-control form-control-user" id="username"
+                                                name="username" aria-describedby="emailHelp"
                                                 placeholder="Enter Email Address...">
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user"
-                                            name="password"
-                                                id="password" placeholder="Password">
+                                                name="password" id="password" placeholder="Password">
                                         </div>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
--- app/Views/user/template/content.php
+++ app/Views/user/template/content.php
@@ -1,81 +1,147 @@
- <!-- content -->
-    <main class="container-fluid">
-        <!-- carousel -->
-        <div class="container">
-            <div id="carouselExampleInterval" class="carousel slide mt-5" data-bs-ride="carousel">
-                <div class="carousel-inner rounded shadow">
-                    <div class="carousel-item active" data-bs-interval="10000">
-                        <img width="1280" height="400"
-                            src="https://810bf2a659dbe63f18a7-974cbc13ede5a0c8fe2137f6c1d2be33.ssl.cf6.rackcdn.com/file/2024-11-20/SKiouFlUiXNI.webp"
-                            class="d-block rounded object-fit-cover w-100" alt="...">
-                    </div>
-                    <div class="carousel-item" data-bs-interval="2000">
-                        <img width="1280" height="400"
-                            src="https://810bf2a659dbe63f18a7-974cbc13ede5a0c8fe2137f6c1d2be33.ssl.cf6.rackcdn.com/file/2024-11-18/BdqBU63OQQpW.webp"
-                            class="d-block rounded object-fit-cover w-100" alt="...">
+<!-- content -->
+<main class="container-fluid">
+    <!-- carousel -->
+    <div class="container">
+        <div id="carouselExampleInterval" class="carousel slide mt-5" data-bs-ride="carousel">
+            <div class="carousel-inner rounded shadow">
+                <div class="carousel-item active" data-bs-interval="10000">
+                    <img width="1280" height="400"
+                        src="https://810bf2a659dbe63f18a7-974cbc13ede5a0c8fe2137f6c1d2be33.ssl.cf6.rackcdn.com/file/2024-11-20/SKiouFlUiXNI.webp"
+                        class="d-block rounded object-fit-cover w-100" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="2000">
+                    <img width="1280" height="400"
+                        src="https://810bf2a659dbe63f18a7-974cbc13ede5a0c8fe2137f6c1d2be33.ssl.cf6.rackcdn.com/file/2024-11-18/BdqBU63OQQpW.webp"
+                        class="d-block rounded object-fit-cover w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+    <!-- carousel end -->
+
+    <!-- kategori -->
+    <div class="row mt-5">
+        <div class="col-xl-3 col-md-6 mb-4">
+            <a href="<?= site_url('beranda/kategori/sosial') ?>">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    Sosial</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">null</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="bi bi-globe-americas" style="font-size: 2rem;"></i>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
-                    data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
-                    data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
+            </a>
         </div>
-        <!-- carousel end -->
 
-        <div class="container shadow rounded mt-5">
-            <ul class="navbar-nav d-flex flex-row justify-content-between gap-5">
-                <li class="nav-item">
-                    <a class="nav-link fs-3" href="<?= site_url('beranda/kategori/sosial')?>"><i class="bi bi-globe-americas"></i> Sosial</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link fs-3"href="<?= site_url('beranda/kategori/pendidikan')?>"><i class="bi bi-mortarboard"></i> Pendidikan</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link fs-3" href="<?= site_url('beranda/kategori/lingkungan')?>"><i class="bi bi-bank"></i> Lingkungan</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link fs-3" href="<?= site_url('beranda/kategori/keagamaan')?>"><i class="bi bi-tree"></i> Keagamaan</a>
-                </li>
-            </ul>
-        </div>
-        
-        <!-- daftar kampanye -->
-        <div class="container mt-5 shadow p-4">
-            <h3 class="">Daftar Kampanye</h3>
-            <div class="container row justify-content-evenly">
-                <?php foreach ($donasi as $data): ?>
-                    <?php $width = round($data['persentase'] ?? 0)?>
-                    
-                    <div class="card shadow m-2" style="width: 30rem; height: 35rem;">
-                        <img src="<?= base_url('assets/uploads/' . $data['gambar']) ?>"
-                            class="card-img-top img-fluid mt-2 rounded object-fit-cover" style="height: 180px; width: 100%;"
-                            alt="<?= $data['gambar'] ?>">
-                        <div class="card-body overflow-auto">
-                            <h5 class="card-title fw-bold"><?= $data['judul'] ?></h5>
-                            <p class="card-text"><?= $data['deskripsi_singkat'] ?></p>
-                        </div>
-                        <div class="card-footer rounded mb-2 d-flex flex-column gap-3">
-                            <div>
-                            <div class="info-dana d-flex flex-column">
-                                <span>Target: Rp. <?= number_format($data['target_donasi'], 0, ',', '.')?></span>
-                                <span>Terkumpul: Rp.<?= number_format($data['terkumpul'], 0, ',', '.')?></span>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <a href="<?= site_url('beranda/kategori/pendidikan') ?>">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Pendidikan</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">null</div>
+
                             </div>
-                            <div class="progress" role="progressbar" aria-label="Info example" aria-valuenow="<?= $data['persentase']?>"
-                                aria-valuemin="0" aria-valuemax="100">
-                                <div class="progress-bar text-bg-info" style="width: <?= $width?>%"><?= $data['persentase']?>%</div>
+                            <div class="col-auto">
+                                <i class="bi bi-mortarboard" style="font-size: 2rem;"></i>
                             </div>
-                            </div>
-                            <a href="<?= site_url('beranda/detail_kampanye/'.$data['id'])?>" class="btn btn-primary">Detail</a>
                         </div>
                     </div>
-                <?php endforeach ?>
-            </div>
+                </div>
+            </a>
         </div>
-    </main> 
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <a href="<?= site_url('beranda/kategori/lingkungan') ?>">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Lingkungan
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">null</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="bi bi-Tree" style="font-size: 2rem;"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <a href="<?= site_url('beranda/kategori/keagamaan') ?>">
+                <div class="card border-left-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                    Keagamaan</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">null</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="bi bi-bank" style="font-size: 2rem;"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <!-- daftar kampanye -->
+    <div class="container mt-5 shadow p-4">
+        <h3 class="">Daftar Kampanye</h3>
+        <div class="container row justify-content-evenly">
+            <?php foreach ($donasi as $data): ?>
+                <?php $width = round($data['persentase'] ?? 0) ?>
+
+                <div class="card shadow m-2" style="width: 30rem; height: 35rem;">
+                    <img src="<?= base_url('assets/uploads/' . $data['gambar']) ?>"
+                        class="card-img-top img-fluid mt-2 rounded object-fit-cover" style="height: 180px; width: 100%;"
+                        alt="<?= $data['gambar'] ?>">
+                    <div class="card-body overflow-auto">
+                        <h5 class="card-title fw-bold"><?= $data['judul'] ?></h5>
+                        <p class="card-text"><?= $data['deskripsi_singkat'] ?></p>
+                    </div>
+                    <div class="card-footer rounded mb-2 d-flex flex-column gap-3">
+                        <div>
+                            <div class="info-dana d-flex flex-column">
+                                <span>Target: Rp. <?= number_format($data['target_donasi'], 0, ',', '.') ?></span>
+                                <span>Terkumpul: Rp.<?= number_format($data['terkumpul'], 0, ',', '.') ?></span>
+                            </div>
+                            <div class="progress" role="progressbar" aria-label="Info example"
+                                aria-valuenow="<?= $data['persentase'] ?>" aria-valuemin="0" aria-valuemax="100">
+                                <div class="progress-bar text-bg-info" style="width: <?= $width ?>%">
+                                    <?= $data['persentase'] ?>%
+                                </div>
+                            </div>
+                        </div>
+                        <a href="<?= site_url('beranda/detail_kampanye/' . $data['id']) ?>"
+                            class="btn btn-primary">Detail</a>
+                    </div>
+                </div>
+            <?php endforeach ?>
+        </div>
+    </div>
+</main>
